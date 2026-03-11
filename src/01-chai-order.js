0c8bf8400bd43e3faca012f067167b0a932f70d4
Rajan Kumar <rajanchamp98@gmail.com>
2026-03-11 22:59:29 +0530

    solved 1

--- src/01-chai-order.js
+++ src/01-chai-order.js
@@ -47,20 +47,44 @@
  */
 export function getChaiOrderLength(order) {
   // Your code here
+  if(typeof order !='string') return -1
+  const trimmedOrder=order.trim()
+  const lengthOrder=trimmedOrder.length
+  return lengthOrder
 }
 
 export function shoutChaiOrder(order) {
   // Your code here
+  if(typeof order !="string") return ""
+  if(order.trim()=="") return ""
+  return order.trim().toUpperCase()
 }
 
 export function whisperChaiOrder(order) {
   // Your code here
+  if(typeof order !="string") return ""
+  if(order.trim() == "") return ""
+  return order.trim().toLowerCase()
 }
 
 export function hasSpecialIngredient(order, ingredient) {
   // Your code here
+  if(typeof order != "string" || typeof ingredient !="string" ) return false
+  const lowerCaseOfOrder=order.trim().toLowerCase()
+  const lowerCaseOfIngredient=ingredient.trim().toLowerCase()
+  if(lowerCaseOfOrder.includes(lowerCaseOfIngredient)){
+    return true
+  }
+  return false
+  
 }
 
 export function getFirstAndLastChar(order) {
   // Your code here
+  if(typeof order !="string" || order.trim()=="") return null
+  const first=order.trim().charAt(0)
+  const last=order.trim().at(-1)
+  return{
+    first,last
+  }
 }
